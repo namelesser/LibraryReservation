badadde7bce15e494f9dedbc727bbef65fe92f01
hzy <4216952@qq.com>
2021-11-11 18:13:35 +0800

    添加 抢座时间,定时抢座

--- LibraryReservation.py
+++ LibraryReservation.py
@@ -1,14 +1,26 @@
 import re
+import time
 import base64
+import sys
+
 import requests
 from bs4 import BeautifulSoup
 import threading
 
 
+class User:
+    def __init__(self, username, password, roomId, seatIdList):
+        self.username = username
+        self.password = password
+        self.name = "未登录"
+        self.roomId = roomId
+        self.seatIdList = seatIdList
+
+
 class LibraryReservation:
 
-    def __init__(self):
-        self.name = ''
+    def __init__(self, user):
+        self.user = user
         self.host = 'http://172.16.47.84/'
         self.session = requests.session()
         headers = {
@@ -43,23 +55,23 @@ class LibraryReservation:
         code = response.json()['words_result'][0]['words']
         return code
 
-    def logLibrary(self, account, password):  # 登录
+    def logLibrary(self):  # 登录
         url_log = 'http://172.16.47.84/'
         data_log = {
             '__VIEWSTATE': self.VIEWSTATE,
             '__EVENTVALIDATION': self.EVENTVALIDATION,
             '__VIEWSTATEGENERATOR': self.VIEWSTATEGENERATOR,
-            'TextBox1': account,
-            'TextBox2': password,
+            'TextBox1': self.user.username,
+            'TextBox2': self.user.password,
             'Button1': '%E7%99%BB++++++%E5%BD%95'
         }
 
-        # 读取教室信息
+        # 读取登录信息
         post_log = self.session.post(url_log, data=data_log)
         if '帐号或密码错误' in post_log.text:
             return '帐号或密码错误'
         elif '齐大图书馆座位预约系统预约导航' in post_log.text:
-            print(self.getName())
+            self.getName()
             return '登录成功'
         else:
             return '未知错误' + post_log.text
@@ -107,7 +119,7 @@ class LibraryReservation:
         else:
             return '预约成功'
 
-    def getddlRoom(self, ddlRoom, ddlDay):  # 获取教室预约人数 ddlDay=[今日|明日]
+    def getddlRoom(self, ddlRoom, ddlDay):  # 获取教室座位 ddlDay=[今日|明日]
         ddlRoom = ddlRoom + "001"
         url_post = 'http://172.16.47.84/DayNavigation.aspx'
         headers = {
@@ -121,21 +133,17 @@ class LibraryReservation:
         post = self.session.post(url=url_post, data=data1, headers=headers)
         post.encoding = 'utf-8'
 
-        bs_psot = BeautifulSoup(post.text, 'lxml')
-        select = bs_psot.select_one('.text input[name=txtSeats]')
+        bs_post = BeautifulSoup(post.text, 'lxml')
+        select = bs_post.select_one('.text input[name=txtSeats]')
         return select['value']
 
     def getName(self):  # 获取号主姓名
-        if not self.name:
-            url = 'http://172.16.47.84/Top.aspx'
-            get = self.session.get(url)
-            get.encoding = 'utf-8'
-            bs_get = BeautifulSoup(get.text, 'lxml')
-            select_name = bs_get.select_one('#Label1')
-            self.name = select_name.text
-            return self.name
-        else:
-            return self.name
+        url = 'http://172.16.47.84/Top.aspx'
+        get = self.session.get(url)
+        get.encoding = 'utf-8'
+        bs_get = BeautifulSoup(get.text, 'lxml')
+        select_name = bs_get.select_one('#Label1')
+        self.user.name = select_name.text
 
     def StartSeatSelection(self, roomid):  # 获取座位信息
         seatId = {}
@@ -215,6 +223,7 @@ class LibraryReservation:
 
 
 def loadAccount():
+    userList = []
     with open("account.txt", "r") as f:
         for line in f.readlines():
             line = line.strip('\n')
@@ -223,69 +232,113 @@ def loadAccount():
             password = list[1]
             roomId = list[2]
             seatIdList = list[3:]
-            myThread1 = myThread(username, password, roomId, seatIdList)
-            myThread1.start()
+            user = User(username, password, roomId, seatIdList)
+            userList.append(user)
+    return userList
 
 
-class myThread(threading.Thread):
-    def __init__(self, username, password, roomId='1201', seatIdList=None):
+class MyThread(threading.Thread):
+    def __init__(self, user, appointment):
+        self.appointment = appointment
         threading.Thread.__init__(self)
-        self.username = username
-        self.password = password
-        self.roomId = roomId
-        self.seatIdList = seatIdList
+        self.user = user
 
     def run(self):
-        lr = LibraryReservation()
-        log = lr.logLibrary(self.username, self.password)
+        lr = LibraryReservation(self.user)
+        log = lr.logLibrary()
         if log != '登录成功':
-            print(self.username + ": 账户或者密码错误")
+            print(self.user.username + ": 账户或者密码错误")
             return log
         rooms = lr.getddlDay()
         try:
-            room_name = rooms[self.roomId]
+            room_name = rooms[self.user.roomId]
         except KeyError:
             print("房间号填写错误,请参照文档.")
             return
 
-        flag = True;
-        while flag:
-            seat_number = self.seatIdList.__len__()
-            for i in range(0, seat_number):
-                if i >= len(self.seatIdList):
+        while True:
+            seat_number = self.user.seatIdList.__len__()
+            for i in range(0, seat_number ):
+                if i >= len(self.user.seatIdList):
                     continue
                 try:
-                    rt = lr.appointmentTomorrow(self.roomId, self.seatIdList[i])
+                    if self.appointment.__eq__("1"):
+                        rt = lr.appointment(self.user.roomId, self.user.seatIdList[i])
+                    else:
+                        rt = lr.appointmentTomorrow(self.user.roomId, self.user.seatIdList[i])
                 except KeyError:
-                    print(self.username + " 预约座位号 " + self.seatIdList[i] + " 失败! 原因:" + self.seatIdList[
+                    print(self.user.username + " 预约座位号 " + self.user.seatIdList[i] + " 失败! 原因:" + self.user.seatIdList[
                         i] + "可能座位号错误,如果座位号没有填错,请上 github 反馈.")
-                    self.seatIdList.remove(self.seatIdList[i])
+                    self.user.seatIdList.remove(self.user.seatIdList[i])
                     continue
                 if '成功' in rt:
                     print(rt)
-                    print(self.username + " 预约座位号 " + self.seatIdList[i] + " 成功! ")
+                    print(self.user.username + " 预约座位号 " + self.user.seatIdList[i] + " 成功! ")
                     return
                 elif '您已经预约了' in rt:
-                    print(self.username + " 预约座位号 " + self.seatIdList[i] + "失败 原因:" + rt)
+                    print(self.user.username + " 预约座位号 " + self.user.seatIdList[i] + "失败 原因:" + rt)
                     return
                 elif '该座位已经有人预约了' in rt:
-                    print(self.username + " 预约座位号 " + self.seatIdList[i] + "失败 原因:" + rt)
-                    self.seatIdList.remove(self.seatIdList[i])
+                    print(self.user.username + " 预约座位号 " + self.user.seatIdList[i] + "失败 原因:" + rt)
+                    self.user.seatIdList.remove(self.user.seatIdList[i])
                 elif '账户被锁定' in rt:
-                    print(self.username + "预约座位号 " + self.seatIdList[i] + "失败 原因:" + rt)
+                    print(self.user.username + "预约座位号 " + self.user.seatIdList[i] + "失败 原因:" + rt)
                     return
                 else:
-                    print(self.username + " 预约座位号 " + self.seatIdList[i] + "失败 原因:" + rt)
-            if len(self.seatIdList) == 0:
-                print(self.username + " 所有座位已被预约,请重新选座")
+                    print(self.user.username + " 预约座位号 " + self.user.seatIdList[i] + "失败 原因:" + rt)
+            if len(self.user.seatIdList) == 0:
+                print(self.user.username + " 所有座位已被预约,请重新选座")
                 return
+
+
+# 返回 现在时间-目标时间
+def computeTimeDifference():
+    # 格式化成2016-03-20 11:45:39形式
+    nowTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    # 取出时分秒
+    HMS = nowTime.split(' ')[1].split(':')
+
+    # 构造出 当天 19 点 19 分 45 秒
+    # 替换时
+    aimTime = nowTime.replace(HMS[0], '18')
+    # 替换分
+    aimTime = aimTime.replace(HMS[1], '19')
+    # 替换秒
+    aimTime = aimTime.replace(HMS[2], '55')
+    # 返回 现在时间-目标时间
+    print("现在时间:" + nowTime)
+    print("开抢时间:" + aimTime)
+    return time.mktime(time.strptime(nowTime, "%Y-%m-%d %H:%M:%S")) - time.mktime(
+        time.strptime(aimTime, "%Y-%m-%d %H:%M:%S"))
+
+
+def checkTime():
+    # 时间差=现在时间 - 目标时间
+    timeDifference = computeTimeDifference()
+    if timeDifference < 0:
+        print("距离 18:19:55 还剩" + str(-1 * timeDifference / 60) + "分钟,正在等待中...")
+        time.sleep(-1 * timeDifference)
+    print("开始抢座!")
 
 
 def main():
     print("----------------欢迎使用图书馆预约系统---------------------")
     print('-----使用说明:')
+    print('----- 抢当天,立即抢当天')
+    print('----- 抢第二天,如果当前时间没有到18:20,程序进入等待,到 18:20 开始抢座, 如果当前时间已过 18:20 则立即抢座')
     print('----- 输入格式:账号 密码 教室 座位号 座位号 座位号')
-    loadAccount()
+    print('----- 输入 1.抢当天(立即抢)  2.抢第二天(18:20:55 开始抢)')
+    appointmentTime = input()
+    if not appointmentTime.__eq__("1") and not appointmentTime.__eq__("2"):
+        print("输入格式错误,程序结束.")
+        sys.exit()
+    if appointmentTime.__eq__("2"):
+        checkTime()
+
+    userList = loadAccount()
+    for i in range(0, userList.__len__()):
+        myThread = MyThread(userList[i], appointmentTime)
+        myThread.start()
 
 
 if __name__ == '__main__':
